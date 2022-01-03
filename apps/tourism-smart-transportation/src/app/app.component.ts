c1e64c82e3c45958f69763545633bf4cd12c8399
SangDT <dtsangdtd@gmail.com>
2022-01-03 15:29:10 +0700

    initial commit

---
+++ apps/tourism-smart-transportation/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'tourism-smart-transportation-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'tourism-smart-transportation';
+}
