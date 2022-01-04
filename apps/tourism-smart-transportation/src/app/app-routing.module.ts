88ea8df328cbefcbab3892666ca00ca7c8597145
SangDT <dtsangdtd@gmail.com>
2022-01-04 21:57:13 +0700

    fix build again 3

--- apps/tourism-smart-transportation/src/app/app-routing.module.ts
+++ apps/tourism-smart-transportation/src/app/app-routing.module.ts
@@ -1,5 +1,4 @@
 import { AdminPagesModule } from './pages/admin-pages/admin-pages.module';
-import { LoginComponent } from './auth/login/login.component';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
