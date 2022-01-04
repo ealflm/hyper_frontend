b3ffe4eb9462d6654d60d3c173dc67d2cb2c3cd2
SangDT <dtsangdtd@gmail.com>
2022-01-04 20:59:37 +0700

    [UI/UX]Login pages
    implement UI from prototype to web

---
+++ apps/tourism-smart-transportation/src/app/auth/login/login.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'tourism-smart-transportation-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
