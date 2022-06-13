276528181468803f22a83242854f1d72a7682f75
SangDT <dtsangdtd@gmail.com>
2022-06-13 17:19:19 +0700

    fix click on map and show detail

--- apps/tourism-smart-transportation/src/app/services/vehicle.service.ts
+++ apps/tourism-smart-transportation/src/app/services/vehicle.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { environment } from './../../environments/environment';
 import { Injectable } from '@angular/core';
@@ -10,7 +10,16 @@ export class VehicleService {
   apiURLTrackingVehicle = environment.apiURL + 'admin/tracking-vehicle';
   apiURLVehicle = environment.apiURL + 'admin/vehicle';
   constructor(private http: HttpClient) {}
-  getListVehicle(): Observable<any> {
+  getListVehicle(name?: string | null): Observable<any> {
+    return this.http.get(`${this.apiURLVehicle}`);
+  }
+  getListVehicleTracking(): Observable<any> {
     return this.http.get(`${this.apiURLTrackingVehicle}`);
+  }
+  getVehicleById(id: string): Observable<any> {
+    return this.http.get(`${this.apiURLVehicle}/${id}`);
+  }
+  getVehicleTrackingById(id: string): Observable<any> {
+    return this.http.get(`${this.apiURLTrackingVehicle}/${id}`);
   }
 }
