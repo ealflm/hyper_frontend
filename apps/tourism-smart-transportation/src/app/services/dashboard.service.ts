89ddd6889035e36df5fabbb42579eee4592ec8c6
SangDT <dtsangdtd@gmail.com>
2022-07-15 16:05:15 +0700

    intergrated API powerBI

--- apps/tourism-smart-transportation/src/app/services/dashboard.service.ts
+++ apps/tourism-smart-transportation/src/app/services/dashboard.service.ts
@@ -1,3 +1,4 @@
+import { environment } from './../../environments/environment';
 import { ConfigResponse } from './../models/ConfigResponse';
 import { Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
@@ -8,8 +9,13 @@ import { Injectable } from '@angular/core';
 })
 @Injectable()
 export class DashboardService {
+  powerBIAPI = environment.apiURL + 'admin/power-bi';
+
   constructor(private httpClient: HttpClient) {}
   getEmbedConfig(endpoint: string): Observable<ConfigResponse> {
     return this.httpClient.get<ConfigResponse>(endpoint);
+  }
+  getEmbeddedTokenPowerBI(): Observable<any> {
+    return this.httpClient.get(`${this.powerBIAPI}`);
   }
 }
